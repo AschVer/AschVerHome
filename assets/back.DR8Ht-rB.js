626b4df1a9bd4b88147504713d883c72985f6b53
AschVer <AschVer@users.noreply.github.com>
2025-10-22 00:07:23 +0000

    deploy: 677ddb6f27d3c6462131e587b00eb27a7fb8f400

---
+++ assets/back.DR8Ht-rB.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.DhI_KFPP.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
