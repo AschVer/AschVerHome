4e15605abe95b79e03a56da4644583fe81035288
AschVer <AschVer@users.noreply.github.com>
2025-12-15 07:21:41 +0000

    deploy: a7d58b141ec4406b0b0d96bebea29b4819849d2e

---
+++ assets/back.Bn9B03Hk.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.4kKnO6Mu.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
